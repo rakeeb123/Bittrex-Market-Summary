c51e7fdc0b4d1aa818f80df8b5aa92620151d738
Rakeeb <Rakeeb@MacBook-Pro.local>
2019-05-29 01:44:07 -0400

    Set up node and finished w/ API

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+const axios = require('axios');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  axios.get('https://api.bittrex.com/api/v1.1/public/getmarketsummaries')
+  	.then(function(response) {
+  		var lst = response.data.result;
+  		var pct = [];
+  		for (var i = 0; i < lst.length; i++) {
+  			var val = parseFloat(100*(lst[i]["Last"] - lst[i]["PrevDay"]) / lst[i]["PrevDay"]).toFixed(3);
+  			if (isNaN(val)) {
+  				pct.push(0);
+  			}
+  			else {
+				pct.push(val);
+  			}
+  		}
+  		res.render('index', {data: lst, headers: Object.keys(lst[0]), percentGain: pct});
+  	})
+});
+
+module.exports = router;
